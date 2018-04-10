f2357ed97c71ff81a9db496ebb753085823fbe21
JmlSaul <JmlSaul@live.com>
2018-04-10 18:19:58 +0800

    add some templates

---
+++ UmbracoDemo/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UmbracoDemo.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            IQueryable<cc> li = Enumerable.Range(1, 100).Select(i => new cc {Id = i.ToString(), I = i}).AsQueryable();
+
+            IQueryable<IGrouping<string,cc>> gourp = li.GroupBy(c => c.Id);
+
+            var g = gourp.Select(item => item.Key);
+
+
+            return View();
+        }
+    }
+
+    class cc
+    {
+        public string Id { get; set; }
+        public int I { get; set; }
+    }
+}
